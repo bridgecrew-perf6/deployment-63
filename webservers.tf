a0d91e8fd5ae5cf1efba9654daf2644f613471fc
devopsokes <meetismail4lyf@gmail.com>
2022-06-13 09:49:42 -0700

    deploying architeture

---
+++ webservers.tf
@@ -0,0 +1,41 @@
+#Create EC2 Instance
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-0d5eff06f840b45e9"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.web-subnet-1.id
+  user_data              = <<-EOF
+  #! /bin/bash
+  yum update -y
+  yum install -y httpd.x86_64
+  systemctl start httpd.service
+  systemctl enable httpd.service
+  echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+  EOF
+  tags = {
+    Name = "Web Server"
+  }
+
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-0d5eff06f840b45e9"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1b"
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.web-subnet-2.id
+  user_data              =  <<-EOF
+  #! /bin/bash
+  yum update -y
+  yum install -y httpd.x86_64
+  systemctl start httpd.service
+  systemctl enable httpd.service
+  echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = "Web Server"
+  }
+
+}
